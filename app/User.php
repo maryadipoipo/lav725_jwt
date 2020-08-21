78a24e9e156ba57cf9562ae1cfcdd0e940e64cf5
maryadipoipo187@gmail.com <maryadipoipo187@gmail.com>
2020-08-21 09:13:11 +0700

    apply jwt on user model and fixing error "Syntax error or access violation: 1071 Specified key was too long; max key length is 1000 bytes"

--- app/User.php
+++ app/User.php
@@ -36,4 +36,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Apply jwt on user
+     */
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
